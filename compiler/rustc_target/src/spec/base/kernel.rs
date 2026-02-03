65dbbf24ef96014940cb2778eb293ce0398a7a0c
WhyAreAllTheseTaken <48136416+WhyAreAllTheseTaken@users.noreply.github.com>
2026-02-03 14:48:24 +0000

    Use an unspecified environment.

--- compiler/rustc_target/src/spec/base/kernel.rs
+++ compiler/rustc_target/src/spec/base/kernel.rs
@@ -1,8 +1,9 @@
-use crate::spec::{Cc, LinkSelfContainedDefault, LinkerFlavor, Lld, Os, RelocModel, StackProbeType, TargetOptions, cvs};
+use crate::spec::{Cc, Env, LinkSelfContainedDefault, LinkerFlavor, Lld, Os, RelocModel, StackProbeType, TargetOptions, cvs};
 
 pub(crate) fn opts() -> TargetOptions {
     TargetOptions {
         os: Os::Kernel,
+        env: Env::Unspecified,
         linker: Some("rust-lld".into()),
         linker_flavor: LinkerFlavor::Gnu(Cc::No, Lld::Yes),
         relocation_model: RelocModel::Static,
